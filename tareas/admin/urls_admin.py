b1d4fd9b5c9abc1b26b02894a3d0dde51c78e0f8
Elmisa117 <misaelsejasveizaga@gmail.com>
2025-06-04 00:53:45 -0400

    Add basic admin dashboard with personal list

--- tareas/admin/urls_admin.py
+++ tareas/admin/urls_admin.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views_admin import dashboard_view
+from .views_admin import dashboard_view, personal_list
 
 urlpatterns = [
     path('', dashboard_view, name='admin_dashboard'),
+    path('personal/', personal_list, name='personal_list'),
 ]
--- tareas/admin/views_admin.py
+++ tareas/admin/views_admin.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from tareas.models import RegistroProfesional
+
 
 def dashboard_view(request):
     return render(request, 'admin/MenuAdmin.html')
+
+
+def personal_list(request):
+    personal = RegistroProfesional.objects.all()
+    return render(request, 'admin/gestion_personal.html', {'personal': personal})
